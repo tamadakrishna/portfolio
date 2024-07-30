4ed2545d5f21f0c321e895ac956bdd0f533cfcf3
tamadakrishna <tamadakrishnaa@gmail.com>
2024-07-30 21:50:23 +0530

    dev:projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,8 +2,46 @@ import React from 'react';
 import Projects from '@/components/UI/Projects';
 
 function page() {
+  const info = [
+    {
+      project_title:"ECOM (ecommerce)",
+      description:"A dynamic e-commerce full-stack application featuring a responsive front end and a robust back end.",
+      brief:`Developed a comprehensive personal e-commerce project with key features including a secure authentication system,
+            user-friendly shopping, a streamlined checkout process, and an intuitive admin panel for managing products, users, and
+            orders. The project showcases my full-stack development skills and attention to detail in creating a seamless online shopping
+            experience.`,
+      year:"Jan 2024",
+      preview:"https://proecommerce.vercel.app/",
+      code:"https://github.com/tamadakrishna/ECOM",
+      sub:[
+        {
+          title:"Frontend:",
+          content:[
+            "React.js: For building interactive user interfaces.",
+            "Tailwind CSS: For styling and responsive design.",
+            "Axios: For making API requests."
+          ]
+        },
+        {
+          title:"Backend:",
+          content:[
+            "Next.js: For server-side logic",
+            "MongoDB: For data storage.",
+            "Cloudinary: For multi-media storage."
+          ]
+        },
+        {
+          title:"Other Tools:",
+          content:[
+            "Git: For version control",
+            "Jest: For testing",
+          ]
+        }
+      ]
+    }
+  ]
   return (
-    <Projects/>
+    <Projects project={info}/>
   )
 }
 
--- components/UI/Projects.js
+++ components/UI/Projects.js
@@ -1,41 +1,114 @@
 "use client";
+import Image from 'next/image';
 import React,{useState} from 'react'
 
-function Projects() {
+function Projects({project}) {
 
   const [modal, setModal] = useState(false)
+  const [modalInfo, setModalInfo] = useState({});
+
+  const setInfo = (info)=>{
+    setModalInfo(info)
+  }
   
   return (
     <div className="w-full h-full text-black  flex items-center justify-center overflow-scroll no-scrollbar ">
-      <div className={`h-[calc(100%_-_50px)] w-[calc(100%_-_50px)] ${modal ? "hidden" : "block"}` }>
-         <div className={`w-[300px] h-[210px] shadow-[0px_2px_5px_gray] rounded-[10px] text-white text-[12px]  bg-[#2F2D28] cursor-pointer ${modal ? "hidden" : "block"}`} onClick={()=>{setModal(val=>!val)}}>
-            <div className='w-full h-[25px]  flex items-center'>
-              <span className="material-symbols-outlined">today</span>
-              <span>{"Time Line"}</span>
-            </div>
-            <div className='w-full h-[25px] flex items-center'>
-              <span className="material-symbols-outlined">topic</span>
-              <span>{"Project Title"}</span>
-            </div>
-            <div className='w-full h-[110px] '>Project Description</div>
-            <div className='w-full h-[20px] flex items-center '>
-              <span className="material-symbols-outlined">link</span>
-              <span>{"Preview of Project"}</span>  
-            </div>
-            <div className='w-full h-[20px] flex items-center '>
-              <span className="material-symbols-outlined">link</span>
-              <span>{"Project Code"}</span>  
-            </div>
-         </div>
-      </div>
+      {
+        project?.map((info,index)=>{
+          return(
+              <div key={index} className={`h-[calc(100%_-_50px)] w-[calc(100%_-_50px)] ${modal ? "hidden" : "block"}` }>
+                <div className={`w-[300px] h-[210px] shadow-[0px_2px_5px_gray] rounded-[10px] text-white text-[12px]  bg-[#2F2D28] ${modal ? "hidden" : "block"}`} >
+                  <div className='w-full h-[25px]  flex items-center'>
+                    <span className="material-symbols-outlined">today</span>
+                    <span>{info?.year}</span>
+                  </div>
+                  <div className='w-full h-[25px] flex items-center'>
+                    <span className="material-symbols-outlined">topic</span>
+                    <span>{info?.project_title}</span>
+                  </div>
+                  <div className='w-full h-[60px] p-2'>
+                    <div className='w-full h-full'>
+                      {info?.description}
+                    </div>
+                  </div>
+                  <div className='w-full h-[50px] flex justify-center items-center'>
+                   <div className='w-[calc(100%_-_120px)] h-[calc(100%_-_20px)] cursor-pointer rounded-[10px] bg-slate-300 flex justify-center items-center border-2' 
+                      onClick={()=>{
+                        setInfo(info);
+                        setModal(val=>!val)
+                        }}>
+                    <span className='text-black'>Click here for more info</span>
+                   </div>
+                  </div>
+                  <div className='w-full h-[20px] flex items-center '>
+                    <span className="material-symbols-outlined">link</span>
+                    <span><a href={info?.preview} target='_blank'>Preview of Project</a></span>  
+                  </div>
+                  <div className='w-full h-[20px] flex items-center '>
+                    <span className="material-symbols-outlined">link</span>
+                    <span><a href={info?.code} target='_blank'>Source Code</a></span> 
+                  </div>
+                </div>
+              </div>
+          )
+        })
+      }
       {/* Modal */}
-      <div className={`h-[calc(100%_-_50px)] w-[calc(100%_-_50px)] flex  border-2 rounded-[5px] bg-white ${modal ? "block" : "hidden"}`}>
-        <div className='w-full h-[30px]  flex justify-end'> 
-          <span className="material-symbols-outlined h-full w-[30px] text-red-500 cursor-pointer" onClick={()=>{setModal(val=>!val)}}>close</span>
-        </div>
+      <div className={`h-[calc(100%_-_50px)] w-[calc(100%_-_50px)] flex flex-col  rounded-[5px] bg-white ${modal ? "block" : "hidden"}`}>
+                <div className='w-full h-[30px]  flex justify-end'> 
+                  <span className="material-symbols-outlined h-full w-[30px] text-red-500 cursor-pointer" onClick={()=>{setModal(val=>!val)}}>close</span>
+                </div>
+                <div className='w-full h-[calc(100%_-_30px)] flex '>
+                  <div className='w-[50%] h-full flex justify-center items-center '>
+                        <div className="w-[calc(100%_-_50px)] h-[calc(100%_-_50px)]  rounded-[5px]  p-4  bg-white relative ">
+                              <Image
+                                  className=" "
+                                  src={'/images/projects/ecom/ecom.png'}
+                                  alt="Product"
+                                  fill={true}
+                              />
+                          </div>
+                  </div>
+                  <div className='w-[50%] h-full'>
+                    <div className='w-full h-[60px]'><span className='text-[40px]'>{modalInfo?.project_title }</span></div>
+                    <div className='w-full p-2 '>
+                      <span className='text-[15px]'>
+                        {modalInfo?.brief}
+                      </span>
+                    </div>
+                    <div className='p-2'>
+                      <div>
+                        <h1 className='text-[20px] font-bold'>Technologies Used</h1>
+                      </div>
+                      <>
+                            {
+                              modalInfo?.sub?.map((info,index)=>{
+                                return (
+                                  <div key={index}>
+                                    <h1 className='text-[15px] font-bold'>{info?.title}</h1>
+                                    {
+                                      info?.content?.map((sub_info,index)=>{
+                                        return (
+                                          <li key={index} className='ml-[20px]'>{sub_info}</li>
+                                        )
+                                      })
+                                    }
+                                  </div>
+                                )
+                              })
+                            }
+                      </>
+                    </div>
+                  </div>
+
+                </div>
       </div>
+      
     </div>
   )
 }
 
 export default Projects
+
+
+
